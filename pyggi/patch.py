a30820e4e9489eacdc7646e426e6079469bed28b
Seongmin Lee <nim.gnoes.eel@gmail.com>
2017-09-25 14:51:06 +0900

    [Modify] Modify patch.py according to new content data structure
    Patch.deletions, Patch.insertions becomes list.
    deletion: list of tuple (target_file, target_line)
    insertion: list of tuple (target_file, target_line, insertion_point)
    And those will be turn into set and dictionary at apply function

--- pyggi/patch.py
+++ pyggi/patch.py
@@ -1,6 +1,7 @@
 import random
 import copy
 import subprocess
+import sys
 from .program import Program
 
 class Patch:
@@ -10,10 +11,10 @@ class Patch:
         self.program = program
         self.modified_program = None
         self.test_result = None
-        # A set of lines to delete
-        self.deletions = set()
-        # Key: Insertion point, Value: Queue of lines which should be inserted
-        self.insertions = {}
+        # A list of deletions: (target_file, target_line)
+        self.deletions = []
+        # A list of insertions: (target_file, target_line, insertion_point)
+        self.insertions = []
         self.history = []
         pass
 
@@ -42,13 +43,40 @@ class Patch:
 
     def apply(self):
         if self.program.manipulation_level == 'physical_line':
-            new_contents = []
-            for i in range(len(self.program.contents) + 1):
-                if i in self.insertions:
-                    for target in self.insertions[i]:
-                        new_contents.append(self.program.contents[target])
-                if i < len(self.program.contents) and i not in self.deletions:
-                    new_contents.append(self.program.contents[i])
+            target_file_list = self.program.contents.keys()
+            new_contents = dict(zip(target_file_list, [[]] * len(target_file_list)))
+
+            for target_file in target_file_list:
+                orig_codeline_list = self.program.contents[target_file]
+                new_codeline_list = new_contents[target_file]
+
+                # Create deletions set for target file: (target_file, target_line)
+                target_file_deletions = set(filter(lambda x: x[0] == target_file,
+                                                   self.deletions))
+
+                # Create insertions dict for target file: insertion_point => [target_lines]
+                target_file_insertions = list(filter(lambda x: x[0] == target_file,
+                                                    self.insertions))
+                insertion_points = list(set(map(lambda x: x[2], target_file_insertions)))
+                target_file_insertions = dict(zip(
+                    insertion_points,
+                    list(map(lambda key: list(map(lambda x: x[1],
+                                                  list(filter(lambda y: y[2] == insertion_points,
+                                                              target_file_insertions)))),
+                             insertion_points))
+                ))
+
+                # Gathers the codelines along with applying the patches
+                for i in range(len(orig_codeline_list) + 1):
+                    # Handle insertions
+                    if i in target_file_insertions.keys():
+                        for target_line in target_file_insertions[i]:
+                            new_codeline_list.append(orig_codeline_list[target_line])
+                    # Handle deletions
+                    if i < len(orig_codeline_list) and (target_file, i) not in target_file_deletions:
+                        new_codeline_list.append(orig_codeline_list[i])
+
+            # Write modified program
             self.modified_program = Program.create_program_with_contents(new_contents)
             return self.modified_program
         else:
@@ -57,41 +85,49 @@ class Patch:
 
     def add_random_edit(self):
         if self.program.manipulation_level == 'physical_line':
-            number_of_program_lines = len(self.program.contents)
+            target_file_list = sorted(self.program.contents.keys())
             edit_type = random.choice(['delete', 'copy', 'move'])
             # line number starts from 0
             while True:
-                target_line = random.randrange(0, number_of_program_lines)
-                if not edit_type == 'delete' or target_line not in self.deletions:
+                # Choice according to the probability distribution
+                def weighted_choice(choice_weight_list):
+                    total = sum(weight for choice, weight in choice_weight_list)
+                    ridx = random.uniform(0, total)
+                    upto = 0
+                    for choice, weight in choice_weight_list:
+                        if upto + weight >= ridx:
+                            return choice
+                        upto += weight
+                    assert False, "weighted_choice error."
+
+                # Choose files based on the probability distribution by number of the code lines
+                target_file = weighted_choice(list(map(lambda filename: (filename, len(self.program.contents[filename])),
+                                                   target_file_list)))
+                target_line = random.randrange(0, len(self.program.contents[target_file]))
+                if not edit_type == 'delete' or (target_file, target_line) not in self.deletions:
                     break
             if edit_type == 'delete':
-                self.delete(target_line)
+                self.delete(target_file, target_line)
             elif edit_type == 'copy':
-                insertion_point = random.randrange(0, number_of_program_lines + 1)
-                self.copy(target_line, insertion_point)
+                insertion_point = random.randrange(0, len(self.program.contents[target_file]) + 1)
+                self.copy(target_file, target_line, insertion_point)
             elif edit_type == 'move':
-                insertion_point = random.randrange(0, number_of_program_lines + 1)
-                self.move(target_line, insertion_point)
+                insertion_point = random.randrange(0, len(self.program.contents[target_file]) + 1)
+                self.move(target_file, target_line, insertion_point)
         else:
             print("[Error] invalid manipulation level: {}".format(self.program.manipulation_level))
             sys.exit()
         return
 
-    def delete(self, target):
-        self.history.append("DELETE {}".format(target)) 
-        self.deletions.add(target)
+    def delete(self, target_file, target_line):
+        self.history.append("DELETE {}: {}".format(target_file, target_line))
+        self.deletions.append((target_file, target_line))
 
-    def copy(self, target, insertion_point):
-        self.history.append("COPY {} -> {}".format(target, insertion_point)) 
-        if insertion_point in self.insertions:
-            self.insertions[insertion_point].append(target)
-        else:
-            self.insertions[insertion_point] = [target]
+    def copy(self, target_file, target_line, insertion_point):
+        self.history.append("COPY {}: {} -> {}".format(target_file, target_line, insertion_point))
+        self.insertions.append((target_file, target_line, insertion_point))
 
-    def move(self, target, insertion_point):
-        self.history.append("MOVE {} -> {}".format(target, insertion_point)) 
-        if insertion_point in self.insertions:
-            self.insertions[insertion_point].append(target)
-        else:
-            self.insertions[insertion_point] = [target]
-        self.deletions.add(target)
+    def move(self, target_file, target_line, insertion_point):
+        self.history.append("MOVE {}: {} -> {}".format(target_file, target_line, insertion_point))
+        self.insertions.append((target_file, target_line, insertion_point))
+        self.deletions.append((target_file, target_line))

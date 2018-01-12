92ac552d648fdcc51b5628a3c8346f3141b6a979
Gabin An <agb94@kaist.ac.kr>
2018-01-12 16:59:47 +0900

    add is_valid_pos method

--- ast_test.py
+++ ast_test.py
@@ -18,10 +18,26 @@ def result_parser(result):
         return TestResult(False, None)
 
 
+def is_valid_pos(root, pos):
+    node = root
+    for block, index in pos:
+        if not block in ['body', 'orelse', 'finalbody']:
+            return False
+        if not block in node.__dict__:
+            return False
+        if not index < len(node.__dict__[block]):
+            return False
+        node = node.__dict__[block][index]
+    return True
+
 def replace(root, dst, src):
     # dst = [('body', 4), ('body', 0)]
     # src = None or [('body', 4)]
     import copy
+    if not is_valid_pos(root, dst):
+        return False
+    if src and not is_valid_pos(root, src):
+        return False
     dst_node, src_node = root, root
     for i in range(len(dst) - 1):
         dst_node = dst_node.__dict__[dst[i][0]][dst[i][1]]
@@ -31,10 +47,12 @@ def replace(root, dst, src):
     else:
         src_node = ast.Pass()
     dst_node.__dict__[dst[-1][0]][dst[-1][1]] = copy.deepcopy(src_node)
-
+    return True
 
 def swap(root, a, b):
     import copy
+    if not is_valid_pos(root, a) or not is_valid_pos(root, b):
+        return False
     a_node, b_node = root, root
     for i in range(len(a) - 1):
         a_node = a_node.__dict__[a[i][0]][a[i][1]]
@@ -45,20 +63,25 @@ def swap(root, a, b):
         1]] = copy.deepcopy(b_node.__dict__[b[-1][0]][b[-1][1]]), copy.deepcopy(
             a_node.__dict__[a[-1][0]][a[-1][1]])
     #dst_node.__dict__[dst[-1][0]][dst[-1][1]] = copy.deepcopy(src_node)
+    return True
 
 
 def insert_before(root, dst, src):
     import copy
+    if not is_valid_pos(root, dst) or not is_valid_pos(root, src):
+        return False
     dst_node, src_node = root, root
     for i in range(len(dst) - 1):
         dst_node = dst_node.__dict__[dst[i][0]][dst[i][1]]
     for j in range(len(src)):
         src_node = src_node.__dict__[src[j][0]][src[j][1]]
     dst_node.__dict__[dst[-1][0]].insert(dst[-1][1], copy.deepcopy(src_node))
-
+    return True
 
 def insert_after(root, dst, src):
     import copy
+    if not is_valid_pos(root, dst) or not is_valid_pos(root, src):
+        return False
     dst_node, src_node = root, root
     for i in range(len(dst) - 1):
         dst_node = dst_node.__dict__[dst[i][0]][dst[i][1]]
@@ -66,6 +89,7 @@ def insert_after(root, dst, src):
         src_node = src_node.__dict__[src[j][0]][src[j][1]]
     dst_node.__dict__[dst[-1][0]].insert(dst[-1][1] + 1,
                                          copy.deepcopy(src_node))
+    return True
 
 
 triangle = Program(
@@ -77,7 +101,7 @@ swap(root, [('body', 0)], [('body', 1)])
 replace(root, [('body', 4)], [('body', 0)])
 replace(root, [('body', 3)], None)
 insert_after(root, [('body', 4)], [('body', 1)])
-insert_before(root, [('body', 4)], [('body', 0)])
+insert_before(root, [('body', 4), ('body', 0)], [('body', 0)])
 #replace(root, [('body', 4), ('body', 0)], None)
 #, root.body[1] = root.body[1], root.body[0]
 #root.body[4].body[0] = ast.Pass()

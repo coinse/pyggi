980464daea1f4167a492d4427c6722beabd4d924
Gabin An <agb94@kaist.ac.kr>
2018-01-21 16:43:45 +0900

    Update ast_test file

--- ast_test.py
+++ ast_test.py
@@ -7,6 +7,7 @@ import astor
 import copy
 import random
 
+# Custom parser for the results of pytest
 def result_parser(result):
     import re
     m = re.findall("runtime: ([0-9.]+)", result)
@@ -18,16 +19,23 @@ def result_parser(result):
     else:
         return TestResult(False, None)
 
+# Create new Program instance for 'sample/Triangle_fast_python'
 triangle = Program(
     "sample/Triangle_fast_python", manipulation_level=MnplLevel.AST)
 triangle.print_modification_points('triangle.py')
-weights = [1] * len(triangle.modification_points['triangle.py'])
+
+# Set modification weights
+weights = [0.01] * len(triangle.modification_points['triangle.py'])
+weights[7] = 1.0 # delay() 
 triangle.set_modification_weights('triangle.py', weights)
+
+# Create new Patch
 patch = Patch(triangle)
-patch.add(StmtMoving.create(triangle, method='weighted'))
+valid_edit_operators = [StmtDeletion, StmtMoving, StmtReplacement]
+edit_operator = random.choice(valid_edit_operators)
+patch.add(edit_operator.create(triangle, method='weighted'))
+
+# Print the patch's info, test results, and line differences made by the patch
 print (patch)
-#patch.add(StmtInsertion(('triangle.py', 4), ('triangle.py', 0)))
-#patch.add(StmtInsertion(('triangle.py', 4), ('triangle.py', 1)))
-#patch.add(StmtInsertion(('triangle.py', 4), ('triangle.py', 2)))
 print (patch.run_test(timeout=30, result_parser=result_parser))
 print (patch.diff)
